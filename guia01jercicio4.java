4a27fa996c7203f7c3fc11fd992fb7b0fe234604
Thomas <thomasmastrogiacomo1@gmail.com>
2026-03-25 23:53:32 -0300

    complete ejercicio 4

--- guia01jercicio4.java
+++ guia01jercicio4.java
@@ -1,10 +1,16 @@
-
-/*
 import java.util.Scanner;
 
 public class guia01jercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Ingrese la medida del primer angulo del triángulo: ");
+        double angulo1 = scanner.nextDouble();
+        System.out.print("Ingrese la medida del segundo angulo del triángulo: ");
+        double angulo2 = scanner.nextDouble();
+        double angulo3 = 180 - (angulo1 + angulo2);
 
-    
+        System.out.println("La medida del tercer angulo del triángulo es: " + angulo3);
+        scanner.close();    
+    }
 }
-*/
