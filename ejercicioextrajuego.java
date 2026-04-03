4cd153216e972086f2831240a9a68e118de9f718
Thomas <thomasmastrogiacomo1@gmail.com>
2026-04-02 23:40:02 -0300

    a ver ahora

---
+++ ejercicioextrajuego.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class ejercicioextrajuego {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("ingrese el nivel de dificultad");
+        System.out.print("Nivel 1 - Facil - Rango del 1 al 10 - Intentos 5");
+        System.out.print("Nivel 2 - Medio - Rango del 1 al 50 - Intentos 7");
+        System.out.print("Nivel 2 - Dificil - Rango del 1 al 100 - Intentos 10");
+
+        
+
+        If (sc==1) {
+
+        }
+
+
+
+
+
+
+        sc.close();
+    }
+
+
+    
+}
