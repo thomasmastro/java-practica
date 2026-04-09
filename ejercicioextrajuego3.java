c9ca8aecafd37ff70e65226a0723f0f3fe005aee
Thomas <thomasmastrogiacomo1@gmail.com>
2026-04-09 06:39:18 -0300

    LO ULTIMO

--- ejercicioextrajuego3.java
+++ ejercicioextrajuego3.java
@@ -5,13 +5,17 @@ public class ejercicioextrajuego3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String secreto = ("hola");
+        String secreto = ("holo");
         String palabra = ("");
         int intentos = 5;
+        int contador = 0;
 
         for (int i = 0; i < secreto.length();i++) {
-            palabra = palabra + "_ ";
+            palabra = palabra + "_";
         }
+
+        System.out.println("La palabra tiene "+secreto.length()+ " Letras");
+        System.out.println(palabra.length());
 
         do {
         System.out.println("Palabra: " + palabra);
@@ -19,14 +23,40 @@ public class ejercicioextrajuego3 {
         System.out.print("Ingrese una letra: ");
         char letra = sc.next().charAt(0);
 
+        contador = 0;
+
         for (int i = 0; i < secreto.length();i++) {
-            if (letra == i) {
-                palabra = palabra + letra;
+            
+            if (letra == secreto.charAt(i)) {
+                palabra = palabra.substring(0, i)
+                + letra
+                + palabra.substring(i + 1);
+                contador++;
             }
-            else
-                System.out.println("Letra incorrecta! ");
-                intentos--;
+
+
+            
+
         }
+        if (contador == 0){
+            System.out.println("letra incorrecta");
+            intentos--;
+        }
+        else{
+            System.out.println("Bien!");
+        }
+        if (intentos == 0) {
+            System.out.println("Perdiste! La palabra era : " + secreto);
+            
+        }
+
+
+
+        if (palabra.equals(secreto)){
+            System.out.println("Ganaste! La palabra era: " + secreto);
+            break;
+        }
+
 
         
         
