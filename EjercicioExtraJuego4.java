8931bc58a1ab6af8a65430a4d563b171a5001dc2
Thomas <thomasmastrogiacomo1@gmail.com>
2026-04-14 22:48:04 -0300

    Actualizado

---
+++ EjercicioExtraJuego4.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class ejercicioextrajuega4 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int contlet = 0;
+    int contnum = 0;
+    int contmayus = 0;
+    int contcaractes = 0;
+
+    System.out.print("Ingrese una contraseña ");
+
+    String contra = sc.nextLine();
+    boolean seguridad = false;
+
+    if (contra.length() < 10) {
+        System.out.println("La contraseña debe tener al menos 10 caracteres.");
+        sc.close();
+        return;
+    }
+
+    for (int i = 0 ;i < contra.length(); i++){
+
+        char caract = contra.charAt(i);
+
+        if (Character.isLetter(caract)) {
+            contlet++;
+        }
+
+        if (Character.isDigit(caract)){
+            contnum++;
+        }
+
+        if (Character.isUpperCase(caract)){
+            contmayus++;
+        }
+
+        if (!Character.isLetterOrDigit(caract)) {
+            contcaractes++;
+        }
+
+
+    }
+
+    if (contnum > 0) {
+        seguridad = true;
+    }
+    else{
+        System.out.println("Contraseña insegura, no contiene numeros");
+        sc.close();
+        return;
+    }
+
+    if (contlet > 0) {
+        seguridad = true;
+    }
+    else {
+        System.out.println("Contraseña insegura, no contiene letras");
+        sc.close();
+        return;
+    }
+
+    if (contmayus > 0) {
+        seguridad = true;
+    }
+    else {
+        System.out.println("Contraseña insegura, no contiene mayusculas");
+        sc.close();
+        return;
+    }
+
+    if (contcaractes > 0) {
+        seguridad = true;
+    }
+    else {
+        System.out.println("Contraseña insegura, no contiene caracteres especiales");
+        sc.close();
+        return;
+    }
+
+
+     if (seguridad) {
+            System.out.println("La contraseña es segura");
+        }
+
+
+
+
+
+
+    sc.close();
+}
+
+    
+}
